94304c80658ac74fc5e6da372f063d07ce9c09bb
rudzeris <ruzil1atangulov@gmail.com>
2024-12-09 16:11:22 +1000

    modify main.cpp: min max

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main(){
+	const int size{5};
+	int massive[size]{5,1,2,6,1};
+	
+	int i_min{}, i_max{};
+	// i_min = min_index(massive,size);
+	// i_max = max_index(massive,size);
+	std :: cout << "i_min: " << i_min << "\ti_max: " << i_max << '\n';
 	
 	return 0;
 }
